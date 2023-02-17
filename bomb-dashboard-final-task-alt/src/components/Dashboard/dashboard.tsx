ce9fd86fef297f7849e2742dbe6496e005cd083d
Swapnapratim <swapnapratim4534@gmail.com>
2023-02-17 21:04:14 +0530

    first commit

---
+++ bomb-dashboard-final-task-alt/src/components/Dashboard/dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      // Add your dashboard component code here
+    </div>
+  );
+}
+
+export default Dashboard;
